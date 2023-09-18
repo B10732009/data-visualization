12f509cf17885edd3df653f47604f3df0c1cd3f6
LAPTOP-UKA4UKG5\yuchen <yuchen4667@yahoo.com.tw>
2023-09-18 19:55:40 +0800

    hw1 : add class indicator and dynamic axis

--- hw1/311552013.js
+++ hw1/311552013.js
@@ -9,27 +9,18 @@ let colors = {
     "Iris-virginica": "#5A5AAD"
 };
 
-const width = 350;
-const height = 350;
+let attributeDomains = {
+    "sepal length": [3, 9],
+    "sepal width": [1, 5],
+    "petal length": [0, 8],
+    "petal width": [0, 4],
+};
+
+const width = 500;
+const height = 500;
 const margin = 30;
 
 const svg = d3.select("#scatter-plot-chart");
-
-// render x-axis
-const xScale = d3.scaleLinear()
-    .domain([0, 8])
-    .range([0, width - margin * 2]);
-svg.append("g")
-    .attr("transform", `translate(${margin}, ${height - margin})`)
-    .call(d3.axisBottom(xScale));
-
-// render y-axis
-const yScale = d3.scaleLinear()
-    .domain([0, 8])
-    .range([height - margin * 2, 0]);
-svg.append("g")
-    .attr("transform", `translate(${margin}, ${margin})`)
-    .call(d3.axisLeft(yScale));
 
 // initially render chart
 renderChart();
@@ -41,9 +32,28 @@ function setLabel(index, value) {
 function renderChart() {
     // load csv file
     d3.csv("http://vis.lab.djosix.com:2023/data/iris.csv").then(function (data) {
-        // remove old dots
+        // remove old dots and axis
         svg.selectAll(".dot").remove();
-        
+        svg.selectAll(".axis").remove();
+
+        // add x-axis
+        const xScale = d3.scaleLinear()
+            .domain(attributeDomains[labels["x"]])
+            .range([0, width - margin * 2]);
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", `translate(${margin}, ${height - margin})`)
+            .call(d3.axisBottom(xScale));
+
+        // add y-axis
+        const yScale = d3.scaleLinear()
+            .domain(attributeDomains[labels["y"]])
+            .range([height - margin * 2, 0]);
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", `translate(${margin}, ${margin})`)
+            .call(d3.axisLeft(yScale));
+
         // add new dots
         svg.append("g")
             .selectAll("dot")
@@ -52,7 +62,7 @@ function renderChart() {
             .attr("class", "dot")
             .attr("cx", function (d) { return xScale(d[labels["x"]]) + margin; })
             .attr("cy", function (d) { return yScale(d[labels["y"]]) + margin; })
-            .attr("r", 2.0)
+            .attr("r", 3.5)
             .style("fill", function (d) { return colors[d["class"]]; })
     });
 }
